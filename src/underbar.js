db2fc966a349e8fa3c07571803bca8619aab6ae0
Evan Spiler <monkeysrawesome@gmail.com>
2014-09-09 15:16:06 -0700

    solved _.some WITHOUT using _.every

--- src/underbar.js
+++ src/underbar.js
@@ -211,6 +211,12 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator == undefined) iterator = _.identity;
+    var result = _.reduce(collection, function(current, item) {
+      return (current || iterator(item));
+    }, false);
+    if (Boolean(result) !== false) return true;
+    return false;
   };
 
 
