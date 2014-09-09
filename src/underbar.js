d8be3df358575935a6cdac544018978bc39bfc8f
Evan Spiler <monkeysrawesome@gmail.com>
2014-09-09 11:15:06 -0700

    solved _.invoke, possibly reduce-able

--- src/underbar.js
+++ src/underbar.js
@@ -147,6 +147,10 @@ var _ = {};
   // Calls the method named by functionOrKey on each value in the list.
   // Note: you will nead to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item) {
+      if (typeof(functionOrKey) == 'function') return functionOrKey.apply(item, arguments);
+      else return item[functionOrKey].apply(item, arguments)
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
